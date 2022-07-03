d55c26e6783be4dbdad408d243c8cd4cf0a6f940
Anton Putra <me@antonputra.com>
2022-07-03 16:22:55 +0500

    asd

---
+++ lessons/115/terraform/5-s3-lambda.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello" {
+  function_name = "hello"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello.key
+
+  runtime = "nodejs16.x"
+  handler = "function.handler"
+
+  source_code_hash = data.archive_file.lambda_hello.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello" {
+  name = "/aws/lambda/${aws_lambda_function.hello.function_name}"
+
+  retention_in_days = 14
+}
